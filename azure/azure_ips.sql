7ba474625a7c83cf3249b1ae09853ca4cc89a203
rezmoss <hi@rezmoss.com>
2026-01-11 18:20:55 +0000

    Update IP Lists

--- azure/azure_ips.sql
+++
@@ -1,8 +0,0 @@
--- SQL script to create table `azure_ips` and insert IP addresses
-
-CREATE TABLE IF NOT EXISTS `azure_ips` (
-  `ip_address` VARCHAR(45) NOT NULL,
-  `ip_type` VARCHAR(10) NOT NULL,
-  PRIMARY KEY (`ip_address`)
-);
-
